2024fe06c251312feb87b39359d77d4a02701dbd
Alejo-P <marcelo.pinzon@epn.edu.ec>
2023-12-05 07:51:51 -0500

    Herencia2

--- Principal.java
+++ Principal.java
@@ -1,1 +1,11 @@
+public class Principal
+{
+    public static void main(String[] args)
+    {
+        Empleados E101=new Empleados("Juan", "162534");
+        Operario E102=new Operario("Jose", "87346583");
 
+        System.out.println(E101.getNombre());
+        E102.setOperario();
+    }
+}
