b17dbdb1d075629f61882156c9ca99283f09fc27
Alejo-P <marcelo.pinzon@epn.edu.ec>
2023-12-19 07:40:35 -0500

    Arreglos, conversiones y manejo de errores

---
+++ Clase_19.java
@@ -0,0 +1,17 @@
+public class Clase_19 {
+    public static void main(String[] args) {
+        int [] pares={2, 4, 6, 8, 12, 14, 16, 18};
+        String [] marcas={"Mercedes", "Nissan", "Kia", "Chevrolet", "Toyota", "Chery", "Hyundai", "Renault", "Shineray"};
+        marcas[7]="Buses";
+        System.out.println("La longitud de la arreglo es: "+marcas.length);
+        for (int i=0; i<marcas.length; i++)
+        {
+            System.out.printf("Marca: %s\n",marcas[i]);
+        }
+        //Arreglos de enteros de 2 dimensiones
+        int [][] matriz = {{3,5,7}, {1,2,4}};
+        System.out.println("longitud: "+matriz.length);
+        System.out.println("Accediendo a la direccion de memoria elemento 0 de la matriz: "+ matriz[0]); //Direccion de memoria
+        System.out.println("Accediendo al elemento 0 de la matriz: "+ matriz[0][0]); // Acceder a un elemento
+    }
+}
