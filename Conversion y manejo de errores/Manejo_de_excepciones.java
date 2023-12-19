486a199a8ce60af1ac094264a612a10aa13888cd
Alejo-P <marcelo.pinzon@epn.edu.ec>
2023-12-19 09:24:07 -0500

    Ejercicio manejo

--- Conversion y manejo de errores/Manejo_de_excepciones.java
+++ Conversion y manejo de errores/Manejo_de_excepciones.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class Manejo_de_excepciones {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        IMC();
+    }
+    static public void manejo()
+    {
         /*try
         {
             Este bloque se ejecutara (y generara una excepcion si fuera el caso)
@@ -28,5 +34,38 @@ public class Manejo_de_excepciones {
             System.out.println("Finalizacion del bloque");
         }
         System.out.println("Finalizacion exitosa");
+    }
+    static public void IMC()
+    {
+        Scanner sc = new Scanner(System.in);
+        float peso, altura, imc;
+        try
+        {
+            System.out.print("Ingresa tu masa en KG >");
+            peso = sc.nextFloat();
+            while (peso<=0)
+            {
+                System.out.print("Ingresa un valor positivo >");
+                peso = sc.nextFloat();
+            }
+            System.out.print("Ingrese su estatura en metros >");
+            altura = sc.nextFloat();
+            while (altura <= 1.5)
+            {
+                System.out.print("Ingrese una estatura valida > ");
+                altura = sc.nextFloat();
+            }
+            imc=(float)(peso/(Math.pow(altura,2)));
+            System.out.printf("Tu IMC es %.3f\n",imc);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Ingreso erroneo de valores!");
+        }
+        finally
+        {
+            System.out.println("Fin del programa!");
+        }
+        sc.close();
     }
 }
