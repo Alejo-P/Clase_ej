97b7b011aea3b53a7fa99aecdd03eaf856e1918d
Alejo-P <marcelo.pinzon@epn.edu.ec>
2023-12-19 07:48:33 -0500

    ajuste2

--- Clase_19.java
+++ Clase_19.java
@@ -9,12 +9,12 @@ public class Clase_19 {
             System.out.printf("Marca: %s\n",marcas[i]);
         }
         //Arreglos de enteros de 2 dimensiones
-        int [][] matriz = {{3,5,7}, {1,2,4}};
+        int [][] matriz = {{3,5,7, 4, 3, 7, 3}, {1,2,4, 7, 8, 6, 0}};
         System.out.println("longitud de una fila: "+matriz[0].length); //Longitud de una fila
         System.out.println("longitud: "+matriz.length);
         System.out.println("Accediendo a la direccion de memoria elemento 0 de la matriz: "+ matriz[0]); //Direccion de memoria
         System.out.println("Accediendo al elemento 0 de la matriz: "+ matriz[0][0]); // Acceder a un elemento
-        for (int j=0;j<matriz.length;j++) //Iterar entre cada elemento de la matriz (filas)
+        for (int j=0;j<matriz.length;j++) //Iterar entre cada elemento de la matriz (cada fila de la matriz)
         {
             for (int k=0;k<matriz[j].length;k++) //iterar entre cada elemento de una fila de la matriz
             {
